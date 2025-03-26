1c3fcbc0736d146ad27e305b6c6d340cf239b06d
Gutocx <acx465@gmail.com>
2025-03-26 11:43:40 -0300

    add mais um arquivo de manipulação de strings

---
+++ strings-multiplas.py
@@ -0,0 +1,19 @@
+nome = "Augusto"
+
+mensagem = f"""
+    Olá meu nome é {nome},
+Eu estou aprendendo Python.
+        Essa mensagem tem diferente recuos em sua composição.
+"""
+
+mensagem_2 = """
+    ======== MENU ========
+
+        1 - Depositar
+        2 - Sacar
+        0 - Sair
+
+    ======================
+"""
+
+print(mensagem_2)
