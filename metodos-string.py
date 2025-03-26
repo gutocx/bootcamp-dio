0b55bb6e2877263a1cf423b868c501d41261f5a2
Gutocx <acx465@gmail.com>
2025-03-26 11:37:18 -0300

    add novos arquivos sobre metodos string

---
+++ metodos-string.py
@@ -0,0 +1,21 @@
+# Maiúscula, minúscula e título
+curso = "pYtHoN"
+print(curso)
+print(curso.upper())
+print(curso.lower())
+print(curso.title())
+print()
+
+# Eliminando espaços em branco
+branco = "   Python  "
+print(branco)
+print(branco.strip())
+print(branco.lstrip())
+print(branco.rstrip())
+print()
+
+# Junção e centralização
+juncao = "Python"
+print(juncao)
+print(juncao.center(10, "#"))
+print(".".join(juncao))
