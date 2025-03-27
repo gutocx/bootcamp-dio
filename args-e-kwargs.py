cd37c87755a0c5ee7aba21f568bda1f36f813b0b
Gutocx <acx465@gmail.com>
2025-03-27 15:34:57 -0300

    Removendo arquivos duplicados

--- args-e-kwargs.py
+++
@@ -1,7 +0,0 @@
-def exibir_poema(data_extenso, *args, **kwargs):
-    texto = "\n".join(args)
-    meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
-    mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
-    print(mensagem)
-
-exibir_poema("Quarta-feira, 26 de Março de 2025", "Zen of Python", "Beautiful is better than ugly.", autor="Tim Peters", ano=1999)
--- estrutura-condicional-ternaria.py
+++
@@ -1,6 +0,0 @@
-saldo = 2000
-saque = int(input("Qual seria o valor do saque? "))
-
-status = "Sucesso" if saldo >= saque else "Falha"
-
-print(f"{status} ao realizar o saque!")
